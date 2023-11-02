b169b946b68a3e69535dc7876ca2afeb711c6885
Antrikas <andreas.antoniades17@gmail.com>
2023-11-02 11:47:42 +0200

    JSMC

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log ('it worked')
+function arrSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; ++i){
+        //Thi is how you loop through every element in an array
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(arrSum ([2, 2, 2,]));
