cf261c3be1402b090e596f01c3ec150a325cb29f
Antrikas <andreas.antoniades17@gmail.com>
2023-11-02 14:33:28 +0200

    JSMC

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-function arrSum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; ++i){
+//function progressiveSum(num) {
+   // sum = 0;
+    //for (let i = 1; i <= num; ++i){
         //Thi is how you loop through every element in an array
-        sum = sum + arr[i];
+       // sum = sum + i;
+    //}
+    //return sum;
+//}
+
+///console.log(progressiveSum([4]));
+
+function calTime(seconds) {
+    let timerMinutes = Math.floor (seconds /60);
+    let timerSeconds = seconds % 60;
+
+   if (timerMinutes.toString().length === 1) {
+    timerMinutes = '0' +timerMinutes
     }
-    return sum;
+
+    return timerMinutes + ':' + timerSeconds;
 }
 
-console.log(arrSum ([2, 2, 2,]));
+console.log(calTime([500]));
